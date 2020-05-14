ca378d6685f2d63ae78d98b6a64bd3b94fc0e9e5
Eduardo Hernández Valdez <eduarch42@protonmail.com>
2020-05-14 11:51:34 -0500

    Update main.go

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/codegangsta/cli"
 
-	"github.com/yudai/gotty/backend/localcommand"
-	"github.com/yudai/gotty/pkg/homedir"
-	"github.com/yudai/gotty/server"
-	"github.com/yudai/gotty/utils"
+	"github.com/saucesaft/gotty/backend/localcommand"
+	"github.com/saucesaft/gotty/pkg/homedir"
+	"github.com/saucesaft/gotty/server"
+	"github.com/saucesaft/gotty/utils"
 )
 
 func main() {
